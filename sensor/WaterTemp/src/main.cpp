54eb4d8fe374b358c3de05daf3082147c6d60996
Ercenk Keresteci <ercenk@ercenk.com>
2020-03-10 14:26:06 -0700

    Post to NodeRed

--- sensor/WaterTemp/src/main.cpp
+++ sensor/WaterTemp/src/main.cpp
@@ -1,15 +1,42 @@
 #include <Arduino.h>
+#include <SPI.h>
+
+// Huzzah
 #include <ESP8266WiFi.h>
- 
+#include <ESP8266HTTPClient.h>
+
 const char* ssid     = "skisandpoles";
 const char* password = "orkan3014";
  
-const char* host = "wifitest.adafruit.com";
+const char* host = "homeberry";
+
+// OneWire
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 2 
+
+OneWire oneWire(ONE_WIRE_BUS); 
+DallasTemperature sensors(&oneWire);
+
+String convertAddress(DeviceAddress deviceAddress)
+{
+  String returnValue = "";
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    if (deviceAddress[i] < 16) returnValue += "0";
+    returnValue += String(deviceAddress[i], HEX);
+  }
+
+  return returnValue;
+}
  
 void setup() {
   Serial.begin(115200);
   delay(100);
  
+ // WiFi
+ // *********************
   // We start by connecting to a WiFi network
  
   Serial.println();
@@ -32,6 +59,9 @@ void setup() {
   Serial.println(WiFi.subnetMask());
   Serial.print("Gateway: ");
   Serial.println(WiFi.gatewayIP());
+
+  // OneWire
+   sensors.begin(); 
 }
  
 int value = 0;
@@ -39,35 +69,54 @@ int value = 0;
 void loop() {
   delay(5000);
   ++value;
- 
+  uint8_t sensorCount = sensors.getDeviceCount();
+  sensors.requestTemperatures();
+  float temp;
+  String sensorValues = "[";
+  for (int i = 0; i < sensorCount ; i++){
+      temp = sensors.getTempFByIndex(i);
+      Serial.print(i); Serial.print(" temperature is: "); 
+      Serial.print(temp);
+      DeviceAddress address;
+      sensors.getAddress(address, i);
+
+      sensorValues += "{\"sensor\": \"" + convertAddress(address) + "\", \"index\": " + String(i) + ", \"value\": " + String(temp) + "}";
+      if (i < sensorCount - 1) sensorValues += ",";
+  }
+
+  sensorValues += "]";
+  Serial.println("Payload is:");
+  Serial.println(sensorValues);
+
   Serial.print("connecting to ");
   Serial.println(host);
   
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
-  const int httpPort = 80;
+  const int httpPort = 1880;
   if (!client.connect(host, httpPort)) {
     Serial.println("connection failed");
     return;
   }
   
   // We now create a URI for the request
-  String url = "/testwifi/index.html";
+  String url = "/watertemp";
   Serial.print("Requesting URL: ");
   Serial.println(url);
   
-  // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n");
+  HTTPClient httpClient;
+
+  String postUrl = "http://" + String(host) + ":" +  String(httpPort) + url;
+  
+  httpClient.begin(postUrl);
+  httpClient.addHeader("Content-Type", "application/json"); 
+  auto httpCode = httpClient.POST(sensorValues);
+
+  Serial.println(httpCode);
+  String payload = httpClient.getString(); 
+  Serial.println(payload); //Print request response payload 
+  httpClient.end();
   delay(500);
-  
-  // Read all the lines of the reply from server and print them to Serial
-  while(client.available()){
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  }
-  
-  Serial.println();
-  Serial.println("closing connection");
+
+
 }
