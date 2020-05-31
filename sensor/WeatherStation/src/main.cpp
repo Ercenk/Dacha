3ba504175d98b8754c1043d2e52d9bcb2568caa1
Ercenk Keresteci <ercenk@ercenk.com>
2020-05-31 15:38:39 -0700

    Wifi Manager

--- sensor/WeatherStation/src/main.cpp
+++ sensor/WeatherStation/src/main.cpp
@@ -4,61 +4,59 @@
 #include <Adafruit_Sensor.h>
 #include "Adafruit_TSL2591.h"
 #include <Adafruit_BME280.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
 
-// Example for demonstrating the TSL2591 library - public domain!
+// Wifi manager
+#include <DNSServer.h>        //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h> //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>      //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 
-// connect SCL to I2C Clock
-// connect SDA to I2C Data
-// connect Vin to 3.3-5V DC
-// connect GROUND to common ground
+char hostParam[40];
+
+#define GPIO0 0
 
 Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // pass in a number for the sensor identifier (for your use later)
-Adafruit_BME280 bme; // use I2C interface
+Adafruit_BME280 bme;                           // use I2C interface
 Adafruit_Sensor *bme_temp = bme.getTemperatureSensor();
 Adafruit_Sensor *bme_pressure = bme.getPressureSensor();
 Adafruit_Sensor *bme_humidity = bme.getHumiditySensor();
 
+// READ
+// https://www.instructables.com/id/ESP8266-Pro-Tips/
+// https://www.bakke.online/index.php/2017/06/02/self-updating-ota-firmware-for-esp8266/
 
-
-/**************************************************************************/
-/*
-    Displays some basic information on this sensor from the unified
-    sensor API sensor_t type (see Adafruit_Sensor for more information)
-*/
-/**************************************************************************/
-void displaySensorDetails(void)
+void setup(void)
 {
-  sensor_t sensor;
-  tsl.getSensor(&sensor);
-  Serial.println(F("------------------------------------"));
-  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" lux"));
-  Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" lux"));
-  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution, 4); Serial.println(F(" lux"));  
-  Serial.println(F("------------------------------------"));
-  Serial.println(F(""));
+  pinMode(GPIO0, INPUT);
 
-    bme_temp->printSensorDetails();
-  bme_pressure->printSensorDetails();
-  bme_humidity->printSensorDetails();
+  Serial.begin(115200);
 
-  delay(500);
-}
+  if (tsl.begin())
+  {
+    Serial.println(F("Found a TSL2591 sensor"));
+  }
+  else
+  {
+    Serial.println(F("No sensor found ... check your wiring?"));
+    while (1)
+      ;
+  }
 
-/**************************************************************************/
-/*
-    Configures the gain and integration time for the TSL2591
-*/
-/**************************************************************************/
-void configureSensor(void)
-{
+  if (!bme.begin(BME280_ADDRESS_ALTERNATE))
+  {
+    Serial.println(F("Could not find a valid BME280 sensor, check wiring!"));
+    while (1)
+      delay(10);
+  }
+
+  /* Configure the sensor */
   // You can change the gain on the fly, to adapt to brighter/dimmer light situations
   //tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
-  tsl.setGain(TSL2591_GAIN_MED);      // 25x gain
+  tsl.setGain(TSL2591_GAIN_MED); // 25x gain
   //tsl.setGain(TSL2591_GAIN_HIGH);   // 428x gain
-  
+
   // Changing the integration time gives you a longer time over which to sense light
   // longer timelines are slower, but are good in very low light situtations!
   //tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);  // shortest integration time (bright light)
@@ -67,274 +65,80 @@ void configureSensor(void)
   // tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS);
   // tsl.setTiming(TSL2591_INTEGRATIONTIME_500MS);
   // tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
-
-  /* Display the gain and integration time for reference sake */  
-  Serial.println(F("------------------------------------"));
-  Serial.print  (F("Gain:         "));
-  tsl2591Gain_t gain = tsl.getGain();
-  switch(gain)
-  {
-    case TSL2591_GAIN_LOW:
-      Serial.println(F("1x (Low)"));
-      break;
-    case TSL2591_GAIN_MED:
-      Serial.println(F("25x (Medium)"));
-      break;
-    case TSL2591_GAIN_HIGH:
-      Serial.println(F("428x (High)"));
-      break;
-    case TSL2591_GAIN_MAX:
-      Serial.println(F("9876x (Max)"));
-      break;
-  }
-  Serial.print  (F("Timing:       "));
-  Serial.print((tsl.getTiming() + 1) * 100, DEC); 
-  Serial.println(F(" ms"));
-  Serial.println(F("------------------------------------"));
-  Serial.println(F(""));
 }
 
-
-/**************************************************************************/
-/*
-    Program entry point for the Arduino sketch
-*/
-/**************************************************************************/
-void setup(void) 
+void loop(void)
 {
-  Serial.begin(115200);
-  
-  Serial.println(F("Starting Adafruit TSL2591 Test!"));
-  
-  if (tsl.begin()) 
+  String host;
+
+  if (digitalRead(GPIO0) == LOW)
   {
-    Serial.println(F("Found a TSL2591 sensor"));
-  } 
-  else 
-  {
-    Serial.println(F("No sensor found ... check your wiring?"));
-    while (1);
+    WiFiManager wifiManager;
+    wifiManager.resetSettings();
+    WiFiManagerParameter host_parameter("server", "host", hostParam, 40);
+    wifiManager.addParameter(&host_parameter);
+
+    wifiManager.startConfigPortal();
+    Serial.println("connected...yeey :)");
+    host = host_parameter.getValue();
+    Serial.println(host);
   }
 
-  if (!bme.begin(BME280_ADDRESS_ALTERNATE)) {
-    Serial.println(F("Could not find a valid BME280 sensor, check wiring!"));
-    while (1) delay(10);
-  }
-    
-  /* Display some basic information on this sensor */
-  displaySensorDetails();
-  
-  /* Configure the sensor */
-  configureSensor();
+  Serial.println(host);
 
-  // Now we're ready to get readings ... move on to loop()!
-}
-
-/**************************************************************************/
-/*
-    Shows how to perform a basic read on visible, full spectrum or
-    infrared light (returns raw 16-bit ADC values)
-*/
-/**************************************************************************/
-void simpleRead(void)
-{
-  // Simple data read example. Just read the infrared, fullspecrtrum diode 
-  // or 'visible' (difference between the two) channels.
-  // This can take 100-600 milliseconds! Uncomment whichever of the following you want to read
-  uint16_t x = tsl.getLuminosity(TSL2591_VISIBLE);
-  //uint16_t x = tsl.getLuminosity(TSL2591_FULLSPECTRUM);
-  //uint16_t x = tsl.getLuminosity(TSL2591_INFRARED);
-
-  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
-  Serial.print(F("Luminosity: "));
-  Serial.println(x, DEC);
-}
-
-/**************************************************************************/
-/*
-    Show how to read IR and Full Spectrum at once and convert to lux
-*/
-/**************************************************************************/
-void advancedRead(void)
-{
+  const size_t capacity = JSON_OBJECT_SIZE(7);
+  DynamicJsonDocument doc(capacity);
   // More advanced data read example. Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
   // That way you can do whatever math and comparisons you want!
+  // Light
   uint32_t lum = tsl.getFullLuminosity();
   uint16_t ir, full;
   ir = lum >> 16;
   full = lum & 0xFFFF;
-  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
-  Serial.print(F("IR: ")); Serial.print(ir);  Serial.print(F("  "));
-  Serial.print(F("Full: ")); Serial.print(full); Serial.print(F("  "));
-  Serial.print(F("Visible: ")); Serial.print(full - ir); Serial.print(F("  "));
-  Serial.print(F("Lux: ")); Serial.println(tsl.calculateLux(full, ir), 6);
-}
 
-/**************************************************************************/
-/*
-    Performs a read using the Adafruit Unified Sensor API.
-*/
-/**************************************************************************/
-void unifiedSensorAPIRead(void)
-{
-  /* Get a new sensor event */ 
-  sensors_event_t event;
-  tsl.getEvent(&event);
- 
-  /* Display the results (light is measured in lux) */
-  Serial.print(F("[ ")); Serial.print(event.timestamp); Serial.print(F(" ms ] "));
-  if ((event.light == 0) |
-      (event.light > 4294966000.0) | 
-      (event.light <-4294966000.0))
-  {
-    /* If event.light = 0 lux the sensor is probably saturated */
-    /* and no reliable data could be generated! */
-    /* if event.light is +/- 4294967040 there was a float over/underflow */
-    Serial.println(F("Invalid data (adjust gain or timing)"));
-  }
-  else
-  {
-    Serial.print(event.light); Serial.println(F(" lux"));
-  }
-}
-
-void bmeRead(void)
-{
-   sensors_event_t temp_event, pressure_event, humidity_event;
+  // Heat, humidity, pressure
+  sensors_event_t temp_event, pressure_event, humidity_event;
   bme_temp->getEvent(&temp_event);
   bme_pressure->getEvent(&pressure_event);
   bme_humidity->getEvent(&humidity_event);
-  
-  Serial.print(F("Temperature = "));
-  Serial.print((temp_event.temperature - 2) * 9/5 + 32);
-  Serial.println(" *F");
 
-  Serial.print(F("Humidity = "));
-  Serial.print(humidity_event.relative_humidity);
-  Serial.println(" %");
+  doc["IR"] = ir;
+  doc["Full"] = full;
+  doc["Visible"] = full - ir;
+  doc["Lux"] = tsl.calculateLux(full, ir);
+  doc["Temperature"] = (temp_event.temperature - 1) * 9 / 5 + 32;
+  doc["Humidity"] = humidity_event.relative_humidity;
+  doc["Pressure"] = pressure_event.pressure;
 
-  Serial.print(F("Pressure = "));
-  Serial.print(pressure_event.pressure);
-  Serial.println(" hPa");
+  String sensorValues;
+  serializeJson(doc, sensorValues);
 
-  Serial.println();
-  delay(1000);
+  Serial.println(sensorValues);
+
+  WiFiClient client;
+  const int httpPort = 1880;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  // We now create a URI for the request
+  String url = "/weather";
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  HTTPClient httpClient;
+
+  String postUrl = "http://" + String(host) + ":" +  String(httpPort) + url;
+
+  httpClient.begin(postUrl);
+  httpClient.addHeader("Content-Type", "application/json");
+  auto httpCode = httpClient.POST(sensorValues);
+
+  Serial.println(httpCode);
+  String payload = httpClient.getString();
+  Serial.println(payload); //Print request response payload
+  httpClient.end();
+
+  delay(2000);
 }
-
-/**************************************************************************/
-/*
-    Arduino loop function, called once 'setup' is complete (your own code
-    should go here)
-*/
-/**************************************************************************/
-void loop(void) 
-{ 
-  //simpleRead(); 
-  advancedRead();
-   //unifiedSensorAPIRead();
-  
-   bmeRead();
-  delay(500);
-}
-
-
-// ////////////////
-
-// #include <Arduino.h>
-// #include <SPI.h>
-
-// // Huzzah
-// #include <ESP8266WiFi.h>
-// #include <ESP8266HTTPClient.h>
-
-// const char* ssid     = "*******";
-// const char* password = "*******";
- 
-// const char* host = "*******";
- 
-// void setup() {
-//   Serial.begin(115200);
-//   delay(100);
- 
-//  // WiFi
-//  // *********************
-//   // We start by connecting to a WiFi network
- 
-//   Serial.println();
-//   Serial.println();
-//   Serial.print("Connecting to ");
-//   Serial.println(ssid);
-  
-//   WiFi.begin(ssid, password);
-  
-//   while (WiFi.status() != WL_CONNECTED) {
-//     delay(500);
-//     Serial.print(".");
-//   }
- 
-//   Serial.println("");
-//   Serial.println("WiFi connected");  
-//   Serial.println("IP address: ");
-//   Serial.println(WiFi.localIP());
-//   Serial.print("Netmask: ");
-//   Serial.println(WiFi.subnetMask());
-//   Serial.print("Gateway: ");
-//   Serial.println(WiFi.gatewayIP());
-
-//   // OneWire
-//    sensors.begin(); 
-// }
- 
-// int value = 0;
- 
-// void loop() {
-//   delay(10000);
-//   ++value;
-//   uint8_t sensorCount = sensors.getDeviceCount();
-//   sensors.requestTemperatures();
-//   float temp;
-//   String sensorValues = "[";
-//   for (int i = 0; i < sensorCount ; i++){
-//       temp = sensors.getTempFByIndex(i);
-//       Serial.print(i); Serial.print(" temperature is: "); 
-//       Serial.print(temp);
-//       DeviceAddress address;
-//       sensors.getAddress(address, i);
-
-//       sensorValues += "{\"sensor\": \"" + convertAddress(address) + "\", \"index\": " + String(i) + ", \"value\": " + String(temp) + "}";
-//       if (i < sensorCount - 1) sensorValues += ",";
-//   }
-
-//   sensorValues += "]";
-//   Serial.println("Payload is:");
-//   Serial.println(sensorValues);
-
-//   Serial.print("connecting to ");
-//   Serial.println(host);
-  
-//   // Use WiFiClient class to create TCP connections
-//   WiFiClient client;
-//   const int httpPort = 1880;
-//   if (!client.connect(host, httpPort)) {
-//     Serial.println("connection failed");
-//     return;
-//   }
-  
-//   // We now create a URI for the request
-//   String url = "/watertemp";
-//   Serial.print("Requesting URL: ");
-//   Serial.println(url);
-  
-//   HTTPClient httpClient;
-
-//   String postUrl = "http://" + String(host) + ":" +  String(httpPort) + url;
-  
-//   httpClient.begin(postUrl);
-//   httpClient.addHeader("Content-Type", "application/json"); 
-//   auto httpCode = httpClient.POST(sensorValues);
-
-//   Serial.println(httpCode);
-//   String payload = httpClient.getString(); 
-//   Serial.println(payload); //Print request response payload 
-//   httpClient.end();
-// }
